4c71cbdac56001dcd7fd1a01a57c529d12ff5098
Uzair <47213777+Uzair05@users.noreply.github.com>
2023-01-09 16:13:15 +0800

    Implemented login ability for officers

--- 03_Webserver/dev/databaseServer.py
+++ 03_Webserver/dev/databaseServer.py
@@ -1,13 +1,35 @@
+from typing import Union, Optional
 from flask import Flask, request
-import typing
 import sqlite3
+from lib.value_handlers import * # get functions and class from library
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/login', methods=['GET'])
 def login():
+    officer_id:Optional[str] = request.args.get("officer_id", None, type=str)
+    password:Optional[str] = request.args.get("password", None, type=str)
+
+    if not((officer_id is not None) and (password is not None)):
+        return generateStatus(False, "Officer_id or Password Incorrect")
+        
+    password = hash(str(password))
+
     conn = sqlite3.connect("./database/stolenVehiclesDatabase.db")
+    cursor = conn.execute(f"""
+        SELECT API from OFFICER_LOGIN where OFFICER_ID=\"{officer_id}\" and PASSWORD=\"{password}\";
+    """)
+    res = [row for row in cursor]
+    conn.close()
+
+    if (len(res)==0):
+        return generateStatus(False, "Officer_id or Password Incorrect")
+    else:
+        return generateStatus(True, "Login Success", {"API":res[0][0]})
+    
 
 
 
