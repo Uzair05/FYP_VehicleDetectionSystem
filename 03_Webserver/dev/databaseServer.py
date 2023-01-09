1ab716c3be92e54e514de3ceb4bf7b80c6c975ac
Uzair <47213777+Uzair05@users.noreply.github.com>
2023-01-09 20:00:59 +0800

    Web functionality added (incomplete)
    Some changes made to database structure

--- 03_Webserver/dev/databaseServer.py
+++ 03_Webserver/dev/databaseServer.py
@@ -16,7 +16,7 @@ def login():
     if not((officer_id is not None) and (password is not None)):
         return generateStatus(False, "Officer_id or Password Incorrect")
         
-    password = hash(str(password))
+    password = hash(password)
 
     conn = sqlite3.connect("./database/stolenVehiclesDatabase.db")
     cursor = conn.execute(f"""
@@ -28,16 +28,101 @@ def login():
     if (len(res)==0):
         return generateStatus(False, "Officer_id or Password Incorrect")
     else:
-        return generateStatus(True, "Login Success", {"API":res[0][0]})
+        # update api token for each login.
+        api = genAPI(password, officer_id)
+        conn = sqlite3.connect("./database/stolenVehiclesDatabase.db")
+        conn.execute(f"UPDATE OFFICER_LOGIN SET API=\"{api}\" WHERE OFFICER_ID=\"{officer_id}\"")
+        conn.commit()
+        conn.close()
+        return generateStatus(True, "Login Success", {"api_token":api})
     
+@app.route('/insertCamera', methods=['GET'])
+def insertCamera():
+    officer_api:Optional[str] = request.args.get("api_token", None, type=str)
+    t, m = testOfficerAPI(officer_api)
+    if (not t):
+        return generateStatus(False, m)
+
+    model_number:Optional[str] = request.args.get("model_number", None, type=str)
+    loc_x:Optional[float] = request.args.get("location_x", None, type=float)
+    loc_y:Optional[float] = request.args.get("location_y", None, type=float)
+    loc_name:Optional[str] = request.args.get("location_name", None, type=str)
+
+    if ((loc_x is None) or (loc_y is None) or (loc_name is None)):
+        return generateStatus(False, "Invalid Input")
+
+    api = genAPI_camera()
+    
+    conn = sqlite3.connect("./database/stolenVehiclesDatabase.db")
+    conn.execute(
+        "INSERT INTO CAMERA (MODEL_NUMBER, LOCATION_X, LOCATION_Y, LOCATION_NAME, API) VALUES (?, ?, ?, ?, ?);",
+        (model_number, loc_x, loc_y, loc_name)
+        )
+    conn.commit()
+    conn.close()
+    return generateStatus(True, "")
+
+@app.route('/insertOfficer', methods=['GET'])
+def insertOfficer():
+    officer_api:Optional[str] = request.args.get("api_token", None, type=str)
+    t, m = testOfficerAPI(officer_api)
+    if (not t):
+        return generateStatus(False, m)
+
+    officer_id:Optional[str] = request.args.get("officer_id", None, type=str)
+    password:Optional[str] = request.args.get("password", None, type=str)
+    if ((officer_id is None) or (password is None)):
+        return generateStatus(False, "Invalid Email or Password")
+    
+
+    name:Optional[str] = request.args.get("name", None, type=str)
+    district:Optional[str] = request.args.get("district", None, type=str)
+    if ((name is None) or (district is None)):
+        return generateStatus(False, "Invalid Name or District")
+
+    password = hash(password)
+    api = genAPI(password, officer_id)
+
+    conn = sqlite3.connect("./database/stolenVehiclesDatabase.db")
+    conn.execute(
+        "INSERT INTO OFFICER (OFFICER_ID, NAME, DISTRICT) VALUES (?, ?, ?);", 
+        (officer_id, name, district)
+    )
+    conn.commit()
+    conn.execute(
+        "INSERT INTO OFFICER_LOGIN (OFFICER_ID, PASSWORD, API) VALUES (?, ?, ?);", 
+        (officer_id, password, api)
+    )
+    conn.commit()
+    conn.close()
+
+    
+    
+
+           
+    
+    # conn = sqlite3.connect("./database/stolenVehiclesDatabase.db")
+    # conn.execute(
+    #     "INSERT INTO CAMERA (MODEL_NUMBER, LOCATION_X, LOCATION_Y, LOCATION_NAME) VALUES (?, ?, ?, ?);",
+    #     (model_number, loc_x, loc_y, loc_name)
+    #     )
+    # conn.commit()
+    # conn.close()
+    return generateStatus(True, "")
+
+
 
 
 
 # TODO: put car details
-# TODO: put officer details
-# TODO: put incident report
-# TODO: put camera details
+# TODO: remove car details
+# TODO: put incident report --> TODO: Test for (camera|vehicle); DONE: Test for (officer)
+# TODO: remove incident report 
 
+
+# DONE: put camera details
+# DONE: put officer details
+# DONE: officer login
 
 
 # TODO: get from database for alert
---
+++ 03_Webserver/try.py
@@ -0,0 +1,7 @@
+from dev.lib.value_handlers import *
+
+print(genAPI_camera("Wan chai", 19.7656, -76.90, None))
+print(genAPI_camera("Wan chai", 19.7656, -76.90, "Canon 998"))
+
+print(genAPI(hash("Hello babes"), "898756@"))
+
